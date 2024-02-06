c7b87d0585677d8598225c6c13d3cf5449bcb883
Roar <roar.holte@youblob.com>
2024-02-06 10:49:06 +0000

    test data

--- 2-MultiSignatures/multisig.sh
+++ 2-MultiSignatures/multisig.sh
@@ -3,17 +3,19 @@ cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.para
 cardano-cli transaction build \
   --babbage-era \
   --testnet-magic 2 \
-  --tx-in "0c5d9a4022651487f0bf812dbef4eb21017e9463228709c0ef29c91ed13960d4#0" \
-  --tx-in "b6412bf29aaf8fdb40265767426a66496df8782964d2f5412a74f5c188586532#0" \
-  --tx-out $nami+"200000000" \
-  --change-address $nami \
+  --tx-in "8e0891651ee3e77cbf439be9e763adbf6c1b48285ccd20d7b14a748eda3ebc7e#0" \
+  --tx-in "4d85ed8387f8ed45dae4a7d0a2be5945238b69d9785d54c6882b28794e470507#0" \
+  --tx-in "306bbe60d201dc79369783958fa244e6c142946ef91f10174eabd37c12cc28e2#0" \
+  --tx-in "1de14ad11c29972fb25675c51ed33c7112ecf0ea56b957dd86d5a9396847af26#3" \
+  --tx-out "addr_test1qqas65gr9hq6qjckfse8qy9x7p8hu4z65atyc3se4xw9t76006wjwp6sxleanemn7qjk8wg9dth39ur3r8aywr4s3mxq5cnz6m"+"100000000" \
+  --change-address "addr_test1vqqz4g35263x8lzyxe2q9gmlyskca7emcak53tt7cenekpc8a9u7h" \
   --protocol-params-file protocol.params \
   --out-file multisig.unsigned
 
 cardano-cli transaction sign \
     --tx-body-file multisig.unsigned \
-    --signing-key-file ../../Wallets/Adr01.skey \
-    --signing-key-file ../../Wallets/Adr02.skey \
+    --signing-key-file /config/workspace/repo/src/wallet/receiver107.skey \
+    --signing-key-file /config/workspace/repo/src/wallet/payment108.skey \
     --testnet-magic 2 \
     --out-file multisig.signed
 
--- 4-MetaData/metadataTx.sh
+++ 4-MetaData/metadataTx.sh
@@ -3,15 +3,15 @@ cardano-cli query protocol-parameters --testnet-magic 2 --out-file protocol.para
 cardano-cli transaction build \
   --babbage-era \
   --testnet-magic 2 \
-  --tx-in "4b6bda5d37598571a89dc876885c5a973bb50e405a50a8545c6f65d395176739#2" \
-  --change-address $nami \
+  --tx-in "bba796dec6f1b7899078cb53103a9251cee41c08208cdace92a355760f6776be#1" \
+  --change-address "addr_test1vqqz4g35263x8lzyxe2q9gmlyskca7emcak53tt7cenekpc8a9u7h" \
   --metadata-json-file myData.json \
   --protocol-params-file protocol.params \
   --out-file metadataTx.unsigned
 
 cardano-cli transaction sign \
   --tx-body-file metadataTx.unsigned \
-  --signing-key-file ../../Wallets/Adr01.skey \
+  --signing-key-file /config/workspace/repo/src/wallet/receiver107.skey \
   --testnet-magic 2 \
   --out-file metadataTx.signed
 
